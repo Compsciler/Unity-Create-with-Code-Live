af60c492611d458ff2c900095737be8ab1e415bb
Compsciler <rogerwang2520@gmail.com>
2020-04-06 10:08:05 -0700

    Prototype 2 Day 1

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float horizontalInput;
+    public float speed;
+
+    public float xRange;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        } else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
