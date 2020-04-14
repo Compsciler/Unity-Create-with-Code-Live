fcd42fdfada0b05d368b9772a038820e034d2c10
Compsciler <rogerwang2520@gmail.com>
2020-04-13 22:18:54 -0700

    Prototype 3 Day 1, PP v0.1

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+
+    private bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
