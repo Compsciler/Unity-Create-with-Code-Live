567707dbbc5ae5b50030fde9cd4f10b1ac49b099
Compsciler <rogerwang2520@gmail.com>
2020-04-14 22:48:36 -0700

    PP v0.2
    Added tile/wall rotation mechanic

---
+++ Sicks Feet Apart/Assets/Scripts/RotateTile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTile : MonoBehaviour
+{
+    private List<GameObject> adjacentWalls = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log(gameObject);
+        foreach (GameObject wall in adjacentWalls)
+        {
+            wall.transform.RotateAround(transform.position, Vector3.up, 90f);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        adjacentWalls.Add(collision.gameObject);
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        adjacentWalls.Remove(collision.gameObject);
+    }
+}
