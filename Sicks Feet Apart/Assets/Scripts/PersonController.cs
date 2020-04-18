4563bba3369e01f1c5a38e6f75d7e8718fdb0b68
Compsciler <rogerwang2520@gmail.com>
2020-04-17 21:31:43 -0700

    PP v0.4.0
    Added NavMesh (with Brackeys scripts)
    Tested on "Test Person"
    
    (Can't seem to figure out what "Carve Only Stationary" changes in my project.)

---
+++ Sicks Feet Apart/Assets/Scripts/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PersonController : MonoBehaviour
+{
+    public Camera cam;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
