5364663efbe3598df44d499627add9ad46d81f44
Compsciler <rogerwang2520@gmail.com>
2020-05-05 10:17:01 -0700

    Research and Troubleshooting
    Updated Prototype 1
    NOTE: Was testing beforehand with Prototype 5, nothing major changed there

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,35 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Vehicle Stats")]
-    [SerializeField] float speed = 15;
+    // [SerializeField] float speed = 15;
+    [SerializeField] float power = 10000;
     [SerializeField] float turnSpeed = 40;
 
     private float forwardInput;
     private float horizontalInput;
-  
+
+    Rigidbody playerRb;
+
+    [SerializeField] GameObject centerOfMass;
+
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] float speed;
+    [SerializeField] TextMeshProUGUI gearText;
+    int gear;
+    int gearInterval = 8;
+
+    [SerializeField] List<WheelCollider> allWheels;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // Axis setup
-        forwardInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
+        if (isOnGround())
+        {
+            // Axis setup
+            forwardInput = Input.GetAxis("Vertical");
+            horizontalInput = Input.GetAxis("Horizontal");
 
-        // Move vehicle
-        transform.Translate(Vector3.forward * speed * forwardInput * Time.deltaTime);
-        // transform.Translate(Vector3.right * turnSpeed * horizontalInput * Time.deltaTime);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+            // Move vehicle
+            playerRb.AddRelativeForce(Vector3.forward * power * forwardInput);
+            // transform.Translate(Vector3.forward * speed * forwardInput * Time.deltaTime);
+            // transform.Translate(Vector3.right * turnSpeed * horizontalInput * Time.deltaTime);
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
+            speed = playerRb.velocity.magnitude;
+            int mph = (int)(speed * 2.237f);  // 3.6f for kph
+            speedometerText.text = "Speed: " + mph + " mph";
+
+            if (mph % gearInterval == 0)
+            {
+                gear = mph / gearInterval + 1;
+                gearText.SetText("Gear: " + gear);
+            }
+        }
         Debug.Log("FPS: " + 1 / Time.deltaTime);
+    }
+    bool isOnGround()
+    {
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (!wheel.isGrounded)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
