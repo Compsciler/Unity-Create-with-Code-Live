8bf398ce9947ce9f66d1b07b7a785bdf8f1e939f
Roger Wang <rogerwang2520@gmail.com>
2020-08-30 22:09:10 -0700

    1.0.0 Build 3
    Implemented ad on score upload if not all clear

---
+++ Six Feet Apart/Assets/Scripts/BonusGame/AdManager2.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+using MEC;
+using TMPro;
+
+public class AdManager2 : MonoBehaviour, IUnityAdsListener
+{
+    internal static AdManager2 instance;
+    internal static bool isInitialized = false;
+
+    private bool isTestMode = false;
+    private string placement = "rewardedVideo";
+
+    internal bool isAdCompleted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        if (!isInitialized)
+        {
+            isInitialized = true;
+            Advertisement.AddListener(this);
+            Advertisement.Initialize(Constants.appleGameId, isTestMode);  // iOS SPECIFIC
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator<float> ShowAd()
+    {
+        isAdCompleted = false;
+        if (!Advertisement.IsReady())
+        {
+            yield return Timing.WaitForOneFrame;
+        }
+        Advertisement.Show(placement);
+        Debug.Log("Ad shown");
+    }
+
+    public void ShowAdStartCoroutine()
+    {
+        Timing.RunCoroutine(ShowAd());
+    }
+
+    public void ShowAdIfNotAllClear()
+    {
+        if (PlayerPrefs.GetInt("IsAllClear") == 0)
+        {
+            Timing.RunCoroutine(ShowAd());
+        }
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        Debug.Log("OnUnityAdsFinish");
+        if (showResult == ShowResult.Finished)
+        {
+            isAdCompleted = true;
+            Debug.Log("Ad finished");
+        }
+        else
+        {
+            Debug.Log("Ad error!");
+        }
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        // throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        Debug.Log("OnUnityAdsDidError");
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        // throw new System.NotImplementedException();
+    }
+
+    void OnDestroy()
+    {
+        Advertisement.RemoveListener(this);
+    }
+}
